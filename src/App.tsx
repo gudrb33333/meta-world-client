101f2a7431387085af06bebf917444a04406467c
에단(손형규) <59630175+gudrb33333@users.noreply.github.com>
2022-09-11 15:03:21 +0900

    add route to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,18 @@ import './App.css';
 import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
 import Home from './component/home/Home';
 import Avatar from './component/avatar/Avatar';
+import Room from './component/room/Room';
+import AvatarList from './component/avatar/AvatarList';
 
 function App() {
 	return (
 		<Router>
 			<Routes>
-				<Route path="/" element={<Home />} />
 				<Route path="/avatar" element={<Avatar />} />
+				<Route path="/avatar-list" element={<AvatarList />} />
+				<Route path="/room" element={<Room />} />
+				<Route path="/" element={<Home />} />
+				<Route path="*" element={<Home />} />
 			</Routes>
 		</Router>
 	);
