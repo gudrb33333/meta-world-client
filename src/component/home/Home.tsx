bbfbaf3a71c6350a2510da5f2ee48d72e027d6ad
에단(손형규) <59630175+gudrb33333@users.noreply.github.com>
2022-09-11 13:36:50 +0900

    update home component

--- src/component/home/Home.tsx
+++ src/component/home/Home.tsx
@@ -3,13 +3,26 @@ import './Home.css';
 
 function Home() {
 	return (
-		<div className="home">
-			<Link to="/avatar">
-				<button>아바타 만들기</button>
-			</Link>
-			<Link to="/avatar-list">
-				<button>이미 만들어진 캐릭터 선택</button>
-			</Link>
+		<div className="home fade-in">
+			<div className="home-screen">
+				<div className="home-title text-flicker-in-glow">Meta World</div>
+				<div className="home-button-container">
+					<Link to="/avatar">
+						<button className="home-create-avatar-button pushable slide-in-left">
+							<span className="shadow"></span>
+							<span className="edge"></span>
+							<span className="front">아바타 만들기</span>
+						</button>
+					</Link>
+					<Link to="/avatar-list">
+						<button className="pushable slide-in-right">
+							<span className="shadow"></span>
+							<span className="edge"></span>
+							<span className="front">캐릭터 선택</span>
+						</button>
+					</Link>
+				</div>
+			</div>
 		</div>
 	);
 }
