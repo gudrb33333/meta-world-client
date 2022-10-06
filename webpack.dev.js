72dad6626f2a124454540ecbdadb075a2b11a7ed
에단(손형규) <59630175+gudrb33333@users.noreply.github.com>
2022-10-06 19:52:50 +0900

    comment out the https configuration of webpack.dev.js

--- webpack.dev.js
+++ webpack.dev.js
@@ -7,12 +7,12 @@ module.exports = merge(common, {
     devtool: 'inline-source-map',
     devServer: {
         // progress: true,
-        liveReload: false,
-        https: {
-            key: fs.readFileSync('./src/certs/key.pem', 'utf-8'),
-            cert: fs.readFileSync('./src/certs/cert.pem', 'utf-8')
-        },
-        host: "hubs.local",
+        //liveReload: false,
+        //https: {
+        //    key: fs.readFileSync('./src/certs/key.pem', 'utf-8'),
+        //    cert: fs.readFileSync('./src/certs/cert.pem', 'utf-8')
+        //},
+        //host: "hubs.local",
         //disableHostCheck: true
         historyApiFallback : true , 
     },
