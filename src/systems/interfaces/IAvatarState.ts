aa1ec8bd5f2caefcf16aa5b3f84b78f03d6ef406
에단(손형규) <59630175+gudrb33333@users.noreply.github.com>
2022-12-22 02:29:47 +0900

    Add canFindCloting variable to the IAvatarState interface

--- src/systems/interfaces/IAvatarState.ts
+++ src/systems/interfaces/IAvatarState.ts
@@ -2,6 +2,7 @@ export interface IAvatarState {
 	canFindChairsToEnter: boolean; // Find a suitable car and run towards it
 	canEnterChairs: boolean; // Actually get into the chair
 	canLeaveChairs: boolean;
+	canFindCloting: boolean;
 
 	update(timeStep: number): void;
 	onInputChange(): void;
