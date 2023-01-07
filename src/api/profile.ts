2cae533c06663e3375fd49222aa434fd8279be78
에단(손형규) <59630175+gudrb33333@users.noreply.github.com>
2023-01-07 14:30:25 +0900

    Add findMyProfile function to profile.ts

--- src/api/profile.ts
+++ src/api/profile.ts
@@ -9,18 +9,31 @@ export interface ProfileProps {
 export async function createProfile(profileProps: ProfileProps) {
     const { nickname, publicType, avatarUrl } = profileProps;
     try {
-        const data = await axios.post('/api/v1/profiles', {
+        return await axios.post('/api/v1/profiles', {
             nickname: nickname,
             publicType: publicType,
             avatarUrl: avatarUrl,
         });
-
-        return data;
     } catch(error) {
         if (error.response.status === 403) {
             alert('권한이 없습니다. 다시 로그인 해주세요.');
         } else {
             alert('알 수 없는 에러로 아바타 생성을 실패했습니다.');
+        }
+    }
+}
+
+export async function findMyProfile() {
+    try {
+        const { data }  = await axios.get('/api/v1/profiles/me');
+        return data;
+    } catch(error) {
+        if (error.response.status === 403) {
+            alert('권한이 없습니다. 다시 로그인 해주세요.');
+        } else if (error.response.status === 404) {
+            alert('이미 생성된 프로필이 없습니다. 프로필을 먼저 생성해 주세요.');
+        } else {
+            alert('알 수 없는 에러로 프로필 조회를 실패했습니다.');
         }
     }
 }
