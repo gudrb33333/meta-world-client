0c94a5a8624b7e4b4da9596e4ca6f65322d010ed
에단(손형규) <59630175+gudrb33333@users.noreply.github.com>
2023-05-09 15:33:15 +0900

    Add fullscreen logic when on mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
 import './App.css';
 import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
+import { useEffect } from 'react';
 import Home from './component/home/Home';
 import Avatar from './component/avatar/Avatar';
 import Room from './component/room/Room';
-import Navbar from './component/room/Sidebar';
+import { isIOS, isAndroid } from './utils/isMobile';
+
+import screenfull from 'screenfull';
 
 function App() {
+
+	useEffect(() => {
+		if(isAndroid || isIOS) {
+			const handleClick = () => {
+				if (screenfull.isEnabled) {
+				  screenfull.request().then(() => {
+					console.log('changed to fullscreen');
+				  }).catch((error) => {
+					console.warn('faild changed to fullscreen:', error);
+				  });
+				}
+			};
+
+			document.addEventListener('touchstart', handleClick);
+			
+			return () => {
+				document.removeEventListener('touchstart', handleClick);
+			};
+		}
+	}, []);
+
 	return (
 		<Router>
 			<Routes>
