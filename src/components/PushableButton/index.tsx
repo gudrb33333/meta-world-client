524cd91c31c1255f6e192437bf0d32fea2068bab
에단(손형규) <59630175+gudrb33333@users.noreply.github.com>
2023-05-22 18:11:12 +0900

    Add PushableButton

---
+++ src/components/PushableButton/index.tsx
@@ -0,0 +1,31 @@
+import classNames from 'classnames';
+import styles from './style.module.css';
+
+interface PushableButtonProps {
+    slideDirection: string;
+    content: string;
+    onClick: () => void;
+}
+
+function PushableButton ({
+    slideDirection,
+    content,
+    onClick
+  }: PushableButtonProps) {
+    return(
+        <button
+            className={classNames([
+              styles.RedBlockButton,
+              styles.pushable,
+              styles[slideDirection],
+            ])}
+            onClick={onClick}
+        >
+            <span className={styles.shadow}></span>
+            <span className={styles.edge}></span>
+            <span className={styles.front}>{content}</span>
+        </button>
+    )
+}
+
+export default PushableButton;
