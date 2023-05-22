7f6ef402d4502d86c1835ed892cf6fcd8444e6fe
에단(손형규) <59630175+gudrb33333@users.noreply.github.com>
2023-05-22 14:47:07 +0900

    Add lobby route

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
 import Home from './components/Home';
 import Avatar from './components/Avatar';
 import Room from './components/Room';
+import RoomLobby from './components/RoomLobby';
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
 			<Routes>
 				<Route path="/avatar" element={<Avatar />} />
 				<Route path="/rooms/:roomName" element={<Room />} />
+				<Route path="/lobby" element={<RoomLobby />} />
 				<Route path="/" element={<Home />} />
 				<Route path="*" element={<Home />} />
 			</Routes>
